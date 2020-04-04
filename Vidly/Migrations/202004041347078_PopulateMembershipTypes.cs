ea3f1c58d039f625db5e39a2f785d382c4ded551
david <4504352+northernocean@users.noreply.github.com>
2020-04-04 13:58:11 +0000

    reset migrations

---
+++ Vidly/Migrations/202004041347078_PopulateMembershipTypes.cs
@@ -0,0 +1,29 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("insert into dbo.Genres ([Name]) values ('Action');");
+            Sql("insert into dbo.Genres ([Name]) values ('Comedy');");
+            Sql("insert into dbo.Genres ([Name]) values ('Family');");
+            Sql("insert into dbo.Genres ([Name]) values ('Romance');");
+
+            Sql("insert into MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate, [Name]) values (1,0,0,0,'Pay as You Go');");
+            Sql("insert into MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate, [Name]) values (2,30,1,10,'Monthly');");
+            Sql("insert into MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate, [Name]) values (3,90,3,15,'Quarterly');");
+            Sql("insert into MembershipTypes (Id, SignUpFee, DurationInMonths, DiscountRate, [Name]) values (4,300,12,20,'Annual');");
+
+        }
+
+        public override void Down()
+        {
+            Sql("delete from MembershipTypes;");
+            Sql("delete from Genres;");
+        }
+
+    }
+}
---
+++ Vidly/Migrations/202004041352496_TestData.cs
@@ -0,0 +1,29 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TestData : DbMigration
+    {
+        public override void Up()
+        {
+
+            Sql("insert into Customers (Name, IsSubscribedToNewsletter, MemberShipTypeId, Birthdate) values ('John Smith', 1, 1, '19730304');");
+            Sql("insert into Customers (Name, IsSubscribedToNewsletter, MemberShipTypeId, Birthdate) values ('Mary Williams', 0, 2, null);");
+
+            Sql("insert into Movies ([Name], ReleaseDate, GenreID, DateAdded, NumberInStock) values ('Hangover', '20000101', 2, '20200329', 5)");
+            Sql("insert into Movies ([Name], ReleaseDate, GenreID, DateAdded, NumberInStock) values ('Die Hard', '20000102', 1, '20200329', 6)");
+            Sql("insert into Movies ([Name], ReleaseDate, GenreID, DateAdded, NumberInStock) values ('Terminator', '20000103', 1, '20200329', 8)");
+            Sql("insert into Movies ([Name], ReleaseDate, GenreID, DateAdded, NumberInStock) values ('Toy Story', '20000104', 3, '20200329', 12)");
+            Sql("insert into Movies ([Name], ReleaseDate, GenreID, DateAdded, NumberInStock) values ('Titanic', '20000105', 4, '20200329', 2)");
+
+        }
+
+        public override void Down()
+        {
+            Sql("delete from Customers;");
+            Sql("delete from Movies;");
+        }
+
+    }
+}
