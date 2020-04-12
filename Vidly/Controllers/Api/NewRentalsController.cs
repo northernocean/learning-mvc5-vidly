97d242d5f814d28a9146a2d21a6aeeb40cd050cd
david <4504352+northernocean@users.noreply.github.com>
2020-04-12 14:39:57 +0000

    Intial RentalForm API class

---
+++ Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+
+        [HttpPost]
+        [Authorize(Roles = RoleName.CanManageMovies)]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
